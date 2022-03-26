9e39e83c7a6c6146e75d798acc706ff849179a28
Doris24 <tinabara@gmx.net>
2022-03-26 14:26:39 +0100

    4.8

--- src/api.js
+++ src/api.js
@@ -36,6 +36,12 @@ export const getEvents = async () => {
     return mockData;
   }
 
+  if (!navigator.onLine) {
+    const data = localStorage.getItem("lastEvents");
+    NProgress.done();
+    return data ? JSON.parse(events).events : [];;
+  }
+
   const token = await getAccessToken();
 
   if (token) {
@@ -44,8 +50,8 @@ export const getEvents = async () => {
     const result = await axios.get(url);
     if (result.data) {
       var locations = extractLocations(result.data.events);
-      localStorage.setItem("lastEvents", JSON.stringify(result.data));
-      localStorage.setItem("locations", JSON.stringify(locations));
+      localStorage.setItem('lastEvents', JSON.stringify(result.data));
+      localStorage.setItem('locations', JSON.stringify(locations));
     }
     NProgress.done();
     return result.data.events;
